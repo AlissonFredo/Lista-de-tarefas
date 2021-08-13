83cd07b633e0d09fe314dce47042dab058696579
alisson fredo <douglasalissonsf@gmail.com>
2021-08-13 18:18:26 -0300

    cadastrando uma tarefa no banco de dados

--- app/tarefa.controller.php
+++ app/tarefa.controller.php
@@ -1,4 +1,14 @@
 <?php
-    echo'<pre>';
-    print_r($_POST);
-    echo'</pre>';
+    require_once ('app/tarefa.model.php');
+    require_once ('app/tarefa.service.php');
+    require_once ('app/conexao.php');
+
+
+    $tarefa = new Tarefa();
+    $tarefa->__set('tarefa', $_POST['tarefa']);
+
+    $conexao = new Conexao();
+
+    $tarefaService = new TarefaService($conexao, $tarefa);
+
+    $tarefaService->inserir();
--- app/tarefa.service.php
+++ app/tarefa.service.php
@@ -1,7 +1,19 @@
 <?php
     class TarefaService {
-        public function inserir(){
 
+        private $conexao;
+        private $tarefa;
+
+        public function __construct(Conexao $conexao, Tarefa $tarefa){
+            $this->conexao = $conexao->conectar();
+            $this->tarefa = $tarefa;
+        }
+
+        public function inserir(){
+            $query = 'INSERT INTO tb_tarefas(tarefa) VALUES(:tarefa)';
+            $stmt = $this->conexao->prepare($query);
+            $stmt->bindValue(':tarefa', $this->tarefa->__get('tarefa'));
+            $stmt->execute();
         }
 
         public function recuperar(){
