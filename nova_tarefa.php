050d295f0b64a9953eb30730a294ebd1296261d6
alisson fredo <douglasalissonsf@gmail.com>
2021-08-13 17:31:58 -0300

    adicionando os arquivos de controller para enviar dados via post para o backend

--- nova_tarefa.php
+++ nova_tarefa.php
@@ -1,54 +1,59 @@
 <html>
-	<head>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>App Lista Tarefas</title>
 
-		<link rel="stylesheet" href="css/estilo.css">
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-	</head>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>App Lista Tarefas</title>
 
-	<body>
-		<nav class="navbar navbar-light bg-light">
-			<div class="container">
-				<a class="navbar-brand" href="#">
-					<img src="img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
-					App Lista Tarefas
-				</a>
-			</div>
-		</nav>
+    <link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"
+        integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+</head>
 
-		<div class="container app">
-			<div class="row">
-				<div class="col-md-3 menu">
-					<ul class="list-group">
-						<li class="list-group-item"><a href="index.php">Tarefas pendentes</a></li>
-						<li class="list-group-item active"><a href="#">Nova tarefa</a></li>
-						<li class="list-group-item"><a href="todas_tarefas.php">Todas tarefas</a></li>
-					</ul>
-				</div>
+<body>
+    <nav class="navbar navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+                App Lista Tarefas
+            </a>
+        </div>
+    </nav>
 
-				<div class="col-md-9">
-					<div class="container pagina">
-						<div class="row">
-							<div class="col">
-								<h4>Nova tarefa</h4>
-								<hr />
+    <div class="container app">
+        <div class="row">
+            <div class="col-md-3 menu">
+                <ul class="list-group">
+                    <li class="list-group-item"><a href="index.php">Tarefas pendentes</a></li>
+                    <li class="list-group-item active"><a href="#">Nova tarefa</a></li>
+                    <li class="list-group-item"><a href="todas_tarefas.php">Todas tarefas</a></li>
+                </ul>
+            </div>
 
-								<form>
-									<div class="form-group">
-										<label>Descrição da tarefa:</label>
-										<input type="text" class="form-control" placeholder="Exemplo: Lavar o carro">
-									</div>
+            <div class="col-md-9">
+                <div class="container pagina">
+                    <div class="row">
+                        <div class="col">
+                            <h4>Nova tarefa</h4>
+                            <hr />
 
-									<button class="btn btn-success">Cadastrar</button>
-								</form>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</body>
+                            <form action="tarefa_controller.php" method="post">
+                                <div class="form-group">
+                                    <label>Descrição da tarefa:</label>
+                                    <input type="text" class="form-control" name="tarefa"
+                                        placeholder="Exemplo: Lavar o carro">
+                                </div>
+
+                                <button type="submit" class="btn btn-success">Cadastrar</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
 </html>
