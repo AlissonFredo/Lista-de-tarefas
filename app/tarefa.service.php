4fa79df1e09cef27223310ec1aa5d8dd103f89fa
alisson fredo <douglasalissonsf@gmail.com>
2021-08-13 16:11:52 -0300

    criaçao do projeto - primeiras classes criadas

---
+++ app/tarefa.service.php
@@ -0,0 +1,18 @@
+<?php
+    class TarefaService {
+        public function inserir(){
+
+        }
+
+        public function recuperar(){
+
+        }
+
+        public function atualizar(){
+
+        }
+
+        public function remover(){
+            
+        }
+    }
